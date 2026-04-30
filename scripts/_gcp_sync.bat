a21c7370e3f6f40a5f5ebd7c45dbd8a85866212f
Patrick Jang <patrick.jang@naraeworld.com>
2026-04-30 13:40:16 +0530

    chore: remove temp _gcp_sync.bat helper

--- scripts/_gcp_sync.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-gcloud compute ssh patrick.jang@trading-bot-seoul --project=tradingbot-korea --zone=us-east1-c --command="git -C /home/patrick.jang/Macro-Pulse show 9bfd310 -- .gitignore"
